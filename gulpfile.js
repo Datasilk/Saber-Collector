b727cdde588fc73deb60b92d5e10d256fad10ea7
Mark Entingh <entingh@gmail.com>
2022-11-20 03:36:30 -0600

    updated publish process

--- gulpfile.js
+++ gulpfile.js
@@ -8,22 +8,44 @@ var publish = '../../bin/';
 var root = '../../';
 var version = "0.1";
 
-gulp.task('publish', () => {
+gulp.task('publish:content', () => {
     //copy all Content folders
-    gulp.src([root + 'Content/**',
-        '!' + root + 'Content/Collector/**'
+    return gulp.src([root + 'Content/**',
+    '!' + root + 'Content/Collector/**'
     ]).pipe(gulp.dest(release + 'Content'));
+});
 
+gulp.task('publish:wwwroot', () => {
     //copy all wwwroot folders
-    gulp.src([root + 'wwwroot/**',
-        '!' + root + 'wwwroot/content/collector/**'
+    return gulp.src([root + 'wwwroot/**',
+    '!' + root + 'wwwroot/content/collector/**'
     ]).pipe(gulp.dest(release + 'wwwroot'));
+});
 
+gulp.task('publish:vendors', () => {
+    //copy all wwwroot folders
+    var vendor = 'Vendors/Collector/';
+    return gulp.src([
+        'config.json',
+        'editor.js',
+        'icon.svg',
+        'README.md',
+        'LICENSE',
+        '**/*.html',
+        '**/Profiles/*',
+        '!**/node_modules/**'
+
+    ]).pipe(gulp.dest(release + vendor));
+});
+
+gulp.task('publish:zip', () => {
     //zip contents of release
-    return gulp.src([release
+    return gulp.src([release + '**'
         //remove any unwanted files from release
         //'!' + release + 'web.config'
     ])
         .pipe(zip(app + '-' + version + '.zip'))
         .pipe(gulp.dest(publish));
 });
+
+gulp.task('publish', gulp.series('publish:content', 'publish:wwwroot', 'publish:vendors', 'publish:zip'));
