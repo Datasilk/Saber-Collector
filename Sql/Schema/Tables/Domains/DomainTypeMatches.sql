ccc5da74eec17a1cb360a7b07945f3e6955ada73
Mark Entingh <entingh@gmail.com>
2022-11-22 18:07:56 -0600

    removed config.json, user can create it from config.template.json

---
+++ Sql/Schema/Tables/Domains/DomainTypeMatches.sql
@@ -0,0 +1,11 @@
+﻿BEGIN TRY
+    CREATE TABLE [dbo].[DomainTypeMatches]
+    (
+	    [matchId] INT NOT NULL PRIMARY KEY, 
+        [type] INT NOT NULL DEFAULT -1,
+        [type2] INT NOT NULL DEFAULT -1, 
+        [words] NVARCHAR(MAX), --comma-delimited list of words to match on the homepage
+        [threshold] INT NOT NULL DEFAULT 1 -- minimum number of matches that must be found to succeed
+    )
+END TRY BEGIN CATCH END CATCH
+    GO
--- Sql/Schema/Tables/Domains/Domains.sql
+++ Sql/Schema/Tables/Domains/Domains.sql
@@ -12,12 +12,16 @@
         [www] BIT NOT NULL DEFAULT 0,
         [empty] BIT NOT NULL DEFAULT 0,
         [deleted] BIT NOT NULL DEFAULT 0,
-        [type] INT NOT NULL DEFAULT -1, -- 0 = unused, 1 = website, 2 = ecommerce, 3 = wiki, 4 = blog, 5 = science journal, 6 = SASS, 7 = social network, 8 = advertiser, 9 = search engine, 10 = portfolio
+        [type] INT NOT NULL DEFAULT -1,
         [type2] INT NOT NULL DEFAULT -1, 
         [articles] INT NOT NULL DEFAULT 0,
         [inqueue] INT NOT NULL DEFAULT 0,
         [title] NVARCHAR(128) NOT NULL DEFAULT '',
         [description] NVARCHAR(255) NOT NULL DEFAULT '',
+        [address] NVARCHAR(32) NOT NULL DEFAULT '',
+        [city] NVARCHAR(32) NOT NULL DEFAULT '',
+        [state] VARCHAR(3) NOT NULL DEFAULT '',
+        [zipcode] VARCHAR(12) NOT NULL DEFAULT '',
         [datecreated] DATETIME2 NOT NULL DEFAULT GETUTCDATE(),
         [dateupdated] DATETIME2 NOT NULL DEFAULT GETUTCDATE(),
         [lastchecked] DATETIME2 NOT NULL DEFAULT GETUTCDATE() -- last scraped a URL from the domain
